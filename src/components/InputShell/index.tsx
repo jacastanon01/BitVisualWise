0559b69fd004ba93eaa01c8a16e0faff48d34f8e
Jacob Castanon <jacastanon01@gmail.com>
2024-06-09 03:37:31 -0500

    created component for terminal input

--- src/components/InputShell/index.tsx
+++ src/components/InputShell/index.tsx
@@ -3,7 +3,7 @@ import BitControls from './BitControls';
 function InputShell() {
   return (
     <section className='w-full h-[60svh] bits z-20'>
-      <div className='w-full flex-shrink-0 flex flex-col items-center text-xl md:text-3xl sm:flex-row'>
+      <div className='flex flex-col justify-between items-center bg-slate-400 text-xl md:text-3xl sm:flex-row'>
         <BitControls />
       </div>
     </section>
