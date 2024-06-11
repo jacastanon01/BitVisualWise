fe6e8e163c5c70f207f27c5b43665e28a4dcfc5d
Jacob Castanon <jacastanon01@gmail.com>
2024-06-11 13:55:27 -0500

    favicon update

--- src/components/InputShell/Introduction.tsx
+++ src/components/InputShell/Introduction.tsx
@@ -4,7 +4,7 @@ const Introduction = () => (
     read from right to left. Bitwise operations are performed on the binary
     representation of a number. Our byte starts at 1 and goes all the way up to
     the 8th bit, which is 128 because it is the 8th power of 2. In this demo, I
-    am using one's complement so that the range of our input is from -1 to 128.{' '}
+    am only dealing with positive numbers so the input range is from 0 to 128.
   </p>
 );
 
