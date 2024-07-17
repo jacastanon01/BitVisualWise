4f48923450c4b421a4bf866a678860404565b3f5
Jacob Castanon <jacastanon01@gmail.com>
2024-07-16 23:39:07 -0500

    Added space to Anchor component to clean Introduction

--- src/components/InputShell/Introduction.tsx
+++ src/components/InputShell/Introduction.tsx
@@ -1,19 +1,54 @@
+// import Anchor from '../shared/Anchor';
+
+// const Introduction = () => (
+//   <p className='leading-normal  tracking-tight text-wrap text-sm md:text-base space-y-2'>
+//     <Anchor href='https://en.wikipedia.org/wiki/Bitwise_operation'>
+//       Bitwise operations
+//     </Anchor>{' '}
+//     are performed on the{' '}
+//     <Anchor href='https://learn.circuitverse.org/docs/binary-representation/binary-numbers.html'>
+//       binary
+//     </Anchor>{' '}
+//     representation of a number. A{' '}
+//     <Anchor href='https://www.khanacademy.org/computing/computers-and-internet/xcae6f4a7ff015e7d:digital-information/xcae6f4a7ff015e7d:bits-and-bytes/a/byte-sized-bits'>
+//       byte
+//     </Anchor>{' '}
+//     starts at the right-most bit and goes all the way up to the 8th bit. In this
+//     demo, we are only dealing with positive numbers so the input range is from 0
+//     to 128. When you chose an operation, the result is calulated by starting
+//     from the right bit and comparing it using a thru a{' '}
+//     <Anchor href='https://www.khanacademy.org/computing/computers-and-internet/xcae6f4a7ff015e7d:computers/xcae6f4a7ff015e7d:logic-gates-and-circuits/a/logic-gates'>
+//       logic gate
+//     </Anchor>{' '}
+//   </p>
+// );
+
+// export default Introduction;
+
 import Anchor from '../shared/Anchor';
 
 const Introduction = () => (
-  <p className='leading-normal tracking-tight text-wrap text-sm md:text-base'>
-    Below are two bytes, or 16 bits, and an operand. Enter two numbers numbers
-    and click an operation.{' '}
+  <p className='leading-normal tracking-tight text-sm md:text-base space-y-2'>
     <Anchor href='https://en.wikipedia.org/wiki/Bitwise_operation'>
       Bitwise operations
-    </Anchor>{' '}
-    are performed on the{' '}
+    </Anchor>
+    are performed on the
     <Anchor href='https://learn.circuitverse.org/docs/binary-representation/binary-numbers.html'>
       binary
-    </Anchor>{' '}
-    representation of a number. Our byte starts at 0 and goes all the way up to
-    the 8th bit, which is 128 because it is the 8th power of 2. In this demo, we
-    are only dealing with positive numbers so the input range is from 0 to 128.
+    </Anchor>
+    representation of a number. A
+    <Anchor href='https://www.khanacademy.org/computing/computers-and-internet/xcae6f4a7ff015e7d:digital-information/xcae6f4a7ff015e7d:bits-and-bytes/a/byte-sized-bits'>
+      byte
+    </Anchor>
+    is generally 8 bits. In this demo, we are only dealing with positive numbers
+    so the input range is from 0 to 128. When you choose an operation, the
+    result is calculated by starting from the right bit and comparing it through
+    a
+    <Anchor href='https://www.khanacademy.org/computing/computers-and-internet/xcae6f4a7ff015e7d:computers/xcae6f4a7ff015e7d:logic-gates-and-circuits/a/logic-gates'>
+      logic gate
+    </Anchor>
+    . Bitwise operations, such as AND, OR, and XOR, allow for the direct
+    manipulation of these individual bits within a byte.
   </p>
 );
 
--- src/components/shared/Anchor.tsx
+++ src/components/shared/Anchor.tsx
@@ -10,13 +10,16 @@ const Anchor = ({
   children: React.ReactNode;
 }) => {
   return (
-    <a
-      className={`underline duration-700 text-white hover:text-stroke hover:no-underline hover:text-shellgreen active:opacity-50 hover:transition-all ${className}`}
-      href={href}
-      target='_blank'
-    >
-      {children}
-    </a>
+    <span>
+      {' '}
+      <a
+        className={`underline duration-700 text-white hover:text-stroke hover:no-underline hover:text-shellgreen active:opacity-50 hover:transition-all ${className}`}
+        href={href}
+        target='_blank'
+      >
+        {children}
+      </a>{' '}
+    </span>
   );
 };
 export default Anchor;
